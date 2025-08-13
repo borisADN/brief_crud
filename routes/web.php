fc7bd60b2071b300c2b5f9c2cee8bd68d433a9c5
Boris <edemwomagno@gmail.com>
2025-08-13 14:49:27 +0000

    ajout du fichier readme

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,16 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\AuthController;
 
 
 Route::get('/', [MainController::class, 'login'])->name('login');
-Route::get('/manage_users', [MainController::class, 'manage_users'])->name('users');
-
 Route::post('/', [AuthController::class, 'handle_login'])->name('login.process');
-
+Route::get('/manage_users', [MainController::class, 'manage_users'])->name('users');
 Route::get('/add_user', [AuthController::class, 'add_user'])->name('add_user');
 Route::post('/add_user', [AuthController::class, 'handle_add_user'])->name('add_user.process');
-
-// Si un jour tout est efface dans la base de donnees 
-// l'admin doit se connecter avec is_admin = 1
-// tres important
-
-// identifiant actuels password 0000
-
 Route::get('/edit_user/{id}', [AuthController::class, 'edit_user'])->name('edit_user');
 Route::post('/edit_user/{id}', [AuthController::class, 'handle_edit_user'])->name('edit_user.process');
-route::get('/delete/{id}', [MainController::class, 'delete'])->name('delete_user');
-route::get('/logout', action: [MainController::class, 'logout'])->name('logout');
-
-
-
-
-
-
-
+Route::get('/delete/{id}', [MainController::class, 'delete'])->name('delete_user');
+Route::get('/logout', [MainController::class, 'logout'])->name('logout');
